4ded4d04fbecc05e18615bc2c9721f80ad8e5af7
AndinaZS <alexa.markova@outlook.com>
2022-02-22 23:48:22 +0300

    addded cat listview and detailview

--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 
+
 class Categories(models.Model):
     name = models.CharField(max_length=20, unique=True)
 
--- ads/views.py
+++ ads/views.py
@@ -1,11 +1,10 @@
 from django.http import JsonResponse
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from ads.models import Advert, Categories
 from users.models import User, Location
 
 
 class AdvertListView(ListView):
-    pass
     model = Advert
 
     def get(self, request, *args, **kwargs):
@@ -15,9 +14,10 @@ class AdvertListView(ListView):
         for advert in self.object_list:
             response.append(
                 {'name': advert.name,
+                 'author_id': advert.author_id,
                  'price': advert.price,
                  'description': advert.description,
-                 'is_published': advert.is_published,
+                 'category_id': advert.category_id,
                  }
             )
 
@@ -32,4 +32,42 @@ class AdvertDetailView(DetailView):
     pass
 
 
+class AdvertUpdateView(UpdateView):
+    pass
 
+
+class AdvertDeleteView(DeleteView):
+    pass
+
+
+class CatListView(ListView):
+    model = Categories
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        response = []
+        for category in self.object_list:
+            response.append({'name': category.name})
+
+        return JsonResponse(response, safe=False)
+
+
+class CatDetailView(DetailView):
+    model = Categories
+
+    def get(self, request, *args, **kwargs):
+        category = self.get_object()
+        return JsonResponse({'name': category.name})
+
+
+class CatCreateView(CreateView):
+    pass
+
+
+class CatUpdateView(UpdateView):
+    pass
+
+
+class CatDeleteView(DeleteView):
+    pass
